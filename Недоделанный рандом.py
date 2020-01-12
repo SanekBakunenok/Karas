afb6d304f9ab2d3d81018e2cb961d1131046ca36
SanekBakunenok <58136394+SanekBakunenok@users.noreply.github.com>
2020-01-12 15:15:06 +0300

    Add files via upload

---
+++ Недоделанный рандом.py
@@ -0,0 +1,6 @@
+import random
+chislo = int(input("Первое число"))
+chislo2= int(input("Второе число"))
+kapec=random.randint(chislo,chislo2)
+print(kapec)
+ 
---
+++ корень.py
@@ -0,0 +1,4 @@
+import math
+u=int(input("Пожалуйста,введи число"))
+print(math.sqrt(u))
+      
